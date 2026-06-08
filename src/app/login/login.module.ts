1e5a87107f66ce1d44bcdcc8234387548a095dc1
Zorica <zokamilunovic@gmail.com>
2026-06-08 15:25:31 +0200

    Logika i pocetni izgled stranice

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule } from '@angular/forms'; 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
-
 import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    FormsModule, 
     IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage] 
 })
 export class LoginPageModule {}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  standalone: false,
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  prijaviSe() {
+    if (this.email && this.password) {
+      this.authService.login(this.email, this.password).subscribe({
+        next: (odgovor) => {
+          console.log('Uspešan login:', odgovor);
+          alert('Dobrodošli!');
+          this.router.navigate(['/home']);
+        },
+        error: (greška) => {
+          console.error('Greška pri loginu:', greška);
+          alert('Neuspešna prijava. Proverite podatke.');
+        }
+      });
+    } else {
+      alert('Molimo popunite oba polja.');
+    }
   }
 
+  registrujSe() {
+    if (this.email && this.password) {
+      this.authService.registracija(this.email, this.password).subscribe({
+        next: (odgovor) => {
+          console.log('Uspešna registracija:', odgovor);
+          alert('Nalog je kreiran! Sada se možete prijaviti.');
+        },
+        error: (greška) => {
+          console.error('Greška pri registraciji:', greška);
+          alert('Greška prilikom registracije.');
+        }
+      });
+    } else {
+      alert('Unesite email i lozinku za registraciju.');
+    }
+  }
 }
