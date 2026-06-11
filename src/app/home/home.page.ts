9182377850659ed943a3a779caeb9b163320256b
Nevena <nevena.mitrovic.bgd@gmail.com>
2026-06-11 17:08:56 +0200

    Pocetni izgled forme za rezervacije

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { ReservationService } from '../services/reservation';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
+   styleUrls: ['./home.page.scss'],
   standalone:false,
 })
 export class HomePage {
